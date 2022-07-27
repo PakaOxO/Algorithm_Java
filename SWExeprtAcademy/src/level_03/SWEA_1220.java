f65e1467c41a48c952232c70a978176cd1149a6e
kevin-Arpe <arpeggiogio@gmail.com>
2022-07-27 23:47:57 +0900

    알고리즘 - SWEA(Level_03) 풀이 업데이트

--- SWExeprtAcademy/src/level_03/SWEA_1220.java
+++ SWExeprtAcademy/src/level_03/SWEA_1220.java
@@ -9,25 +9,39 @@ public class SWEA_1220 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		int T = 10;
+		StringBuilder sb = new StringBuilder();
 		for (int i=1; i<=T; i++) {
-			int[][] board = new int[100][];
-			for (int j=0; j<100; j++) {
+			int N = Integer.parseInt(br.readLine());
+			int[][] board = new int[N][N];
+			for (int j=0; j<N; j++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				for (int k=0; k<100; k++) {
+				for (int k=0; k<N; k++) {
 					board[j][k] = Integer.parseInt(st.nextToken());
 				}
 			}
-			for (int row=0; row<100; row++) {
+			int cnt = 0;
+			for (int row=0; row<N; row++) {
 				int col = 0;
-				if (board[col][row] == 1) {
-					do {
-						col++;
-						
-					} while (col < 100);
-				}
+				int[] arr = new int[2];
+				do {
+					if (board[col][row] == 1) arr[0] = 1;
+					else if (board[col][row] == 2) {
+						if (arr[0] == 1) {
+							cnt++;
+							arr[0] = 0;
+							arr[1] = 0;
+						} else {
+							arr[1] = 1;
+						}
+					}
+					col++;
+				} while (col < N);
 			}
+			sb.append("#" + i + " " + cnt + "\n");
 		}
+		System.out.println(sb);
+		br.close();
 	}
 
 }
