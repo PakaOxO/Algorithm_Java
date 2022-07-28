a8d00d4a67ada3cbdeac850ddfc4207745483586
kevin-Arpe <arpeggiogio@gmail.com>
2022-07-28 23:21:58 +0900

    알고리즘 - 백준(로직) 풀이 업데이트

---
+++ BaekJoon/src/logic/BaekJoon_1284.java
@@ -0,0 +1,25 @@
+package logic;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 집 주소
+public class BaekJoon_1284 {
+	public static int[] width = { 4, 2, 3, 3, 3, 3, 3, 3, 3, 3 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String N;
+		while (!(N = br.readLine()).equals("0")) {
+			int total = 1;
+			for (int i=0; i < N.length(); i++) {
+				total += (width[Character.getNumericValue(N.charAt(i))] + 1);
+			}
+			sb.append(total + "\n");
+		}
+		System.out.println(sb.toString().trim());
+		br.close();
+	}
+
+}
