76f5d2f933076ca5aef9a77c9a6b78e74e70a665
kevin-Arpe <arpeggiogio@gmail.com>
2022-08-01 23:15:35 +0900

    알고리즘 - BaekJoon(math) 풀이 업데이트

---
+++ BaekJoon/src/math/BaekJoon_2577.java
@@ -0,0 +1,27 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 숫자의 개수
+public class BaekJoon_2577 {
+	public static int[] cnt = new int[10];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		
+		char[] multiple = Integer.toString(A * B * C).toCharArray();
+		for (char num : multiple) {
+			cnt[Character.getNumericValue(num)]++;
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i : cnt) {
+			sb.append(i + "\n");
+		}
+		System.out.println(sb.toString().trim());
+	}
+
+}
