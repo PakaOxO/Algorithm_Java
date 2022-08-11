60d75c52a7e50ec9a0737e99d1b952a639097cde
kevin-Arpe <57397003+kevin-Arpe@users.noreply.github.com>
2022-08-11 16:24:24 +0900

    알고리즘 - BaekJoon(string) 풀이 업데이트

--- BaekJoon/src/string/BaekJoon_5525.java
+++ BaekJoon/src/string/BaekJoon_5525.java
@@ -3,25 +3,32 @@ package string;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-// IOIOI
+// IOIOI (50Á¡)
 public class BaekJoon_5525 {
-	static String ioi = "IOI";
+	static StringBuilder ioi = new StringBuilder("IOI");
 
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		for (int i=0; i<N; i++) {
-			ioi += "OI";
+		for (int i=1; i<N; i++) {
+			ioi.append("OI");
 		}
 		int M = Integer.parseInt(br.readLine());
 		String str = br.readLine();
 		int ioiLen = ioi.length();
-		for (int i=0; i<str.length(); i++) {
-			for (int j=i+ioiLen; j>=i; j--) {
-				
+		int result = 0;
+		for (int i=0; i<M-ioiLen; i++) {
+			boolean isSame = true;
+			for (int j=0; j<ioiLen; j++) {
+				if (str.charAt(i+j) != ioi.charAt(j)) {
+					isSame = false;
+					break;
+				}
 			}
+			if (isSame) result++;
 		}
+		System.out.println(result);
 	}
 
 }
