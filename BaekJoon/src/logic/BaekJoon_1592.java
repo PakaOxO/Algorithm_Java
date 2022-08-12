8658ddb11f1b555f369a67da0b86a2a8fa4ac90c
kevin-Arpe <57397003+kevin-Arpe@users.noreply.github.com>
2022-08-12 18:02:02 +0900

    알고리즘 - BaekJoon(logic) 풀이 업데이트

---
+++ BaekJoon/src/logic/BaekJoon_1592.java
@@ -0,0 +1,37 @@
+package logic;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 영식이와 친구들
+public class BaekJoon_1592 {
+	
+	static int[] cnt;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		cnt = new int[N];
+		cnt[0]++;
+		
+		int pointer = 0;
+		int loop = 0;
+		while (true) {
+			if (cnt[pointer] == M) break;
+			if (cnt[pointer] % 2 == 0) {
+				pointer = (pointer - L < 0) ? pointer - L + N : pointer - L;
+			} else {
+				pointer = (pointer + L) % N;
+			}
+			cnt[pointer]++;
+			loop++;
+		}
+		System.out.println(loop);
+		br.close();
+	}
+
+}
