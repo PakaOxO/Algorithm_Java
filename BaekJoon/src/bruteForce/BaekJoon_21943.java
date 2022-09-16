4ae8c185489c414ff35cec1634325a812fa8ed8b
kevin-Arpe <arpeggiogio@gmail.com>
2022-09-17 01:33:44 +0900

    알고리즘 - BaekJoon(bruteForce) 풀이 업데이트

--- BaekJoon/src/bruteForce/BaekJoon_21943.java
+++ BaekJoon/src/bruteForce/BaekJoon_21943.java
@@ -8,49 +8,32 @@ import java.util.*;
  * @author kevin-Arpe
  * 
  * Sketch Idea
- * 	1. 
+ * 	1. 먼저 괄호를 통해 모든 덧셈을 계산한 수들로 곱셈 연산을 하는 것이 가장 그리디한 풀이
+ * 	2. 곱셈할 숫자는 최소 (곱셈 연산자 개수) + 1 이므로 완전 탐색을 통해 곱셈할 숫자의 위치에 숫자들의 합을 넣는다.
+ * 	3. 모든 덧셈 연산이 종료되면 곱셈 연산을 수행한 뒤 최대값인지 판별한다.
  *
  */
 public class BaekJoon_21943 {
 	static int N;
 	static int[] nums;
 	static int[] opCnt;
-	static boolean[] comb;
+	static int[] sums;
 	static int max;
 	
-	static void cal() {
-		Deque<Integer> q = new LinkedList<>();
-		for (int num : nums) q.addLast(num);
-		for (int i=0; i<N-1; i++) {
-			if (comb[i]) q.addFirst(q.removeFirst() + q.removeFirst());
-			else q.addLast(q.removeFirst());
-		}
-		System.out.println(q);
-		int result = 0;
-		if (q.size() > 0) result = q.removeFirst();
-		while (q.size() > 0) result *= q.removeFirst();
-		
-		max = Math.max(max, result);
-	}
-	
-	static void dfs(int depth) {
-		if (depth == N - 1) {
-			System.out.println(Arrays.toString(comb));
-			cal();
+	static void dfs(int cnt, int idx) {
+		if (cnt == N) {
+			int multi = sums[0];
+			for (int i=1; i<opCnt[1]+1; i++) {
+				multi *= sums[i];
+			}
+			max = Math.max(max, multi);
 			return;
 		}
 		
-		if (opCnt[0] > 0) {
-			comb[depth] = true;
-			opCnt[0]--;
-			dfs(depth + 1);
-			comb[depth] = false;
-			opCnt[0]++;
-		}
-		if (opCnt[1] > 0) {
-			opCnt[1]--;
-			dfs(depth + 1);
-			opCnt[1]++;
+		for (int i=0; i<opCnt[1] + 1; i++) { 
+			sums[i] += nums[idx];
+			dfs(cnt + 1, idx + 1);
+			sums[i] -= nums[idx];
 		}
 	}
 
@@ -70,9 +53,12 @@ public class BaekJoon_21943 {
 			opCnt[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		comb = new boolean[N - 1];
+		sums = new int[opCnt[1] + 1];
+		sums[0] += nums[0];
+		sums = new int[opCnt[1] + 1];
 		max = 0;
-		dfs(0);
+		
+		dfs(0, 0);
 		
 		System.out.println(max);
 		br.close();
