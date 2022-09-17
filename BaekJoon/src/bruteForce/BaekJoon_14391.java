324e84d8482e8651c6f146897caf59feda1e583a
kevin-Arpe <arpeggiogio@gmail.com>
2022-09-17 11:46:09 +0900

    알고리즘 - BaekJoon(bruteForce) 풀이 업데이트

--- BaekJoon/src/bruteForce/BaekJoon_14391.java
+++ BaekJoon/src/bruteForce/BaekJoon_14391.java
@@ -8,11 +8,12 @@ import java.util.*;
  * @author kevin-Arpe
  * 
  * Sketch Idea
- * 	1. 시작지점에서 선택할 수 있는 방향은 먼저 2가지
- * 		1.1 하나는 가로로 이을 것인가.
- * 		1.2 다른 하나는 세로로 이을 것인가.
- * 	2. 가로 세로를 정했으면 다음은 1개부터 N또는 M까지 또는 이전에 방문했던 칸을 만날 때까지
- * 	3. 모든 칸을 탐색 완료했다면 숫자의 합을 이전에 저장된 최대값과 비교
+ * 	1. 완전 탐색을 통해 먼저 첫 번째 가로 줄을 탐색해 고를 수 있는 조합의 수(1~M개의 숫자)를 선택
+ * 	2. 첫 가로 줄의 탐색이 끝나면 ~N번 가로 줄까지 반복
+ * 	3. 가로 줄 하나하나의 탐색이 끝날 때마다 가로 줄에서 선택된 숫자의 합(이어져 있는 경우 자리수를 늘려가며)을 도출해 sum에 저장
+ * 	4. 모든 가로줄의 탐색이 끝나면 남은 건 세로줄인데 가로 줄 선택에 의해 세로 줄은 자동으로 선택되어짐
+ * 	5. 가로 줄에서 가로세로 1 크기의 숫자는 이미 탐색을 했으므로 세로 줄은 2이상의 숫자만 나올 수 있음
+ * 	6. 세로 줄의 모든 숫자의 합을 기존의 가로 줄 숫자의 합과 더해 최대값과 비교한 뒤 더 크면 저장
  *
  */
 public class BaekJoon_14391 {
@@ -21,62 +22,52 @@ public class BaekJoon_14391 {
 	static boolean[][] isVisited;
 	static int max;
 	
-	static boolean check(int r, int c, int type, int len) {
-		if (type == 0) { // 가로
-			if (len > M - c) return false;
-			for (int i=0; i<len; i++) {
-				if (isVisited[r][c + i]) return false;
+	static int cal(int r, int c, int type) {
+		int sum = 0;
+		int total = 0;
+		if (type == 0) {
+			for (int i=0; i<M; i++) {
+				if (isVisited[r][i]) {
+					sum = sum * 10 + map[r][i];
+				} else {
+					total += sum;
+					sum = 0;
+				}
 			}
-			return true;
-		} else { // 세로
-			if (len > N - r) return false;
-			for (int i=0; i<len; i++) {
-				if (isVisited[r + i][c]) return false;
+			total += sum;
+		} else {
+			for (int j=0; j<M; j++) {
+				sum = 0;
+				for (int i=0; i<N; i++) {
+					if (!isVisited[i][j]) {
+						sum = sum * 10 + map[i][j];
+					} else {
+						if (sum >= 10) {
+							total += sum;
+						}
+						sum = 0;
+					}
+				}
+				total += sum;
 			}
-			return true;
 		}
-	}
-	
-	static int visit(int r, int c, int type, boolean val, int len) {
-		int sum = map[r][c];
-		if (type == 0) { // 가로
-			for (int i=1; i<len; i++) {
-				isVisited[r][c + i] = val;
-				sum = sum * 10 + map[r][c + i];
-			}
-		} else { // 세로
-			for (int i=1; i<len; i++) {
-				isVisited[r + i][c] = val;
-				sum += sum * 10 + map[r + i][c];
-			}
-		}
-		System.out.println(sum);
-		return sum;
+		return total;
 	}
 	
 	static void dfs(int r, int c, int sum) {
-		if (c >= M) {
-			c = 0;
-			r++;
-		}
-		
-		if (r == N) {
-			max = Math.max(max, sum);
+		if (c == M) {
+			dfs(r + 1, 0, sum + cal(r, c, 0));
 			return;
 		}
-		if (isVisited[r][c]) dfs(r, c + 1, sum);
-		
-		for (int len=4; len>0; len--) {
-			if (check(r, c, 0, len)) {
-				dfs(r, c + len, sum + visit(r, c, 0, true, len));
-				visit(r, c, 0, false, len);
-			}
-			
-			if (check(r, c, 1, len)) {
-				dfs(r, c + 1, sum + visit(r, c, 1, true, len));
-				visit(r, c, 1, false, len);
-			}
+		if (r == N) {
+			max = Math.max(max, sum + cal(r, c, 1));
+			return;
 		}
+		
+		isVisited[r][c] = true;
+		dfs(r, c + 1, sum);
+		isVisited[r][c] = false;
+		dfs(r, c + 1, sum);
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -92,9 +83,8 @@ public class BaekJoon_14391 {
 				map[i][j] = (int)(line.charAt(j) - '0');
 			}
 		}
-		
+
 		isVisited = new boolean[N][M];
-		max = 0;
 		dfs(0, 0, 0);
 		
 		System.out.println(max);
