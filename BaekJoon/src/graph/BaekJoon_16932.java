814296f5aeadcd2fc607cbff1c8a7e31451bdf70
kevin-Arpe <arpeggiogio@gmail.com>
2022-09-24 02:46:46 +0900

    알고리즘 - BaekJoon(graph) 풀이 업데이트

--- BaekJoon/src/graph/BaekJoon_16932.java
+++ BaekJoon/src/graph/BaekJoon_16932.java
@@ -4,27 +4,93 @@ import java.io.*;
 import java.util.*;
 
 /**
- * BaekJoon_16932, 모양 만들기
+ * BaekJoon_16932, 모양 만들기(HARD)
  * @author kevin-Arpe
  * 
- * Sketch Idea
- * 	1. 메모리가 넉넉하니 먼저 입력을 받으며 0의 위치를 리스트에 저장
+ * Sketch Idea(시간초과)
+ * 	1. 먼저 입력을 받으며 0의 위치를 리스트에 저장
  * 	2. 리스트에서 임의의 0의 위치를 하나 뽑아 1로 바꿔줌
  * 		2.1 그 상태에서 전체 맵을 순회하면서 1의 위치를 발견했다면 BFS 탐색
  * 		2.2 탐색의 결과 카운팅한 1의 개수와 max값을 비교해 max를 갱신
+ * 
+ * 	1. 입력을 받으면서 0의 위치를 큐에 저장
+ * 	2. 큐에서 0의 위치를 하나씩 빼면서 주변에 1이 있는지 체크
+ * 		2.1 1이 있으면 바로 1의 위치에서 BFS 탐색을 시작해 연결된 1들을 그룹핑(그룹핑 단위는 11부터)
+ * 		2.2 그룹핑을 하면서 해당 그룹의 1의 개수를 카운팅, 그룹핑이 끝나면 해당 그룹핑 번호 - 1의 개수 (key-value) 쌍으로 map에 저장
+ * 		2.3 주변에 1이 하나라도 있었던 0의 위치는 다시 큐에 삽입하는데 삽입하기 전에 다시 사방 탐색하면서 그룹핑된 요소가 있으면 해당 그룹번호를 set에 저장
+ * 		2.4 그룹 번호 set을 돌면서 해당 번호로 다시 Map에서 그룹 개수들을 가져와 sum에 더한 뒤 +1(자기 자신)
+ * 		2.5 큐에 다시 넣기 전에 sum과 max를 비교해서 max값 갱신
  *
  */
 public class BaekJoon_16932 {
+	
 	static class Point {
-		int r, c;
+		int r, c, sum;
 		Point(int r, int c) {
 			this.r = r;
 			this.c = c;
+			this.sum = 0;
 		}
 	}
-	static int N, M;
+	
+	static int N, M, cnt, max;
 	static int[][] map;
-	static List<Point> zero;
+	static Queue<Point> joint;
+	static Map<Integer, Integer> group;
+	static int[][] drc = { { 0, -1 }, { 0, 1 }, { 1, 0 },  { -1, 0 } };
+	
+	static void grouping(int r, int c, int groupingNo) {
+		Queue<Point> q = new LinkedList<>();
+		q.offer(new Point(r, c));
+		map[r][c] = groupingNo;
+		cnt = 1;
+		
+		while(q.size() > 0) {
+			Point curr = q.poll();
+			for (int i=0; i<4; i++) {
+				int nr = curr.r + drc[i][0];
+				int nc = curr.c + drc[i][1];
+				if (!(0 <= nr && nr < N && 0 <= nc && nc < M) || map[nr][nc] != 1) continue;
+				q.offer(new Point(nr, nc));
+				map[nr][nc] = groupingNo;
+				cnt++;
+			}
+		}
+		group.put(groupingNo, cnt);
+	}
+	
+	static void getJoint() {
+		int qSize = joint.size();
+		int groupingNo = 11;
+		for (int i=0; i<qSize; i++) {
+			Point curr = joint.poll();
+			boolean flag = false;
+			Set<Integer> set = new HashSet<>();
+			for (int j=0; j<4; j++) {
+				int nr = curr.r + drc[j][0];
+				int nc = curr.c + drc[j][1];
+				if (!(0 <= nr && nr < N && 0 <= nc && nc < M) || map[nr][nc] == 0) continue;
+				
+				if (map[nr][nc] == 1) {
+					grouping(nr, nc, groupingNo);
+					set.add(groupingNo);
+				} else if (map[nr][nc] > 10) {
+					set.add(map[nr][nc]);
+				}
+				flag = true;
+				groupingNo++;
+			}
+			
+			if (flag) {
+				int sum = 0;
+				Iterator<Integer> it = set.iterator();
+				while (it.hasNext()) sum += group.get(it.next());
+				curr.sum = sum + 1;
+				joint.offer(curr);
+				max = Math.max(max, curr.sum);
+			}
+		}
+	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,13 +98,21 @@ public class BaekJoon_16932 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
+		joint = new LinkedList<>();
+		group = new HashMap<>();
 		
 		for (int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j=0; j<M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 0) joint.offer(new Point(i, j));
 			}
 		}
+		br.close();
+		
+		max = 0;
+		getJoint();
+		System.out.println(max);
 	}
 
 }
--- BaekJoon/src/graph/BaekJoon_2573.java
+++ BaekJoon/src/graph/BaekJoon_2573.java
@@ -4,82 +4,55 @@ import java.io.*;
 import java.util.*;
 
 /**
- * BaekJoon_2573, 빙산
+ * BaekJoon_2573, 빙산(HARD)
  * @author kevin-Arpe
  * 
  * Sketch Idea
- * 	1. 
+ * 	1. 먼저 입력을 받으면서 얼음의 위치를 기억해 노드를 만든 후 큐에 저장
+ * 	2. 반복문을 큐의 크기(얼음의 개수)만큼 돌리면서 사방 탐색
+ * 	3. 바로 녹이지말고 녹은 개수를 노드에 저장한 뒤 해당 분기 끝나면 한번에 녹임(녹아서 다른 얼음을 녹일 때 영향 주는 것 방지)
+ * 	4. 전체 큐를 돌면서 저장된 녹일 개수만큼 map의 높이를 줄여줌 0보다 같거나 작아지면 0으로 보정
+ * 	5. 높이가 0이된 얼음이 있다면 얼음의 위치 중 하나를 가져와서 dfs탐색
+ * 	6. 탐색의 결과 얼음의 개수가 전체 큐의 길이(전체 얼음의 개수)보다 작으면 얼음이 쪼개진 것이므로 해당 날짜를 answer에 저장하고 반복문(while) break
  *
  */
 public class BaekJoon_2573 {
 	static int N, M, cCnt, answer;
 	static int[][] map;
-	static boolean[][][] isVisited;
+	static boolean[][] isVisited;
 	static Queue<Iceberg> queue;
-	static boolean[][] tempVisited;
 	
 	static int[][] drc = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 	
 	static class Iceberg {
-		int r, c, height, depth;
+		int r, c, meltingCnt;
 		
-		Iceberg(int r, int c, int height, int depth) {
+		Iceberg(int r, int c, int meltingCnt) {
 			this.r = r;
 			this.c = c;
-			this.height = height;
-			this.depth = depth;
+			this.meltingCnt = meltingCnt;
 		}
 	}
 	
-	static boolean check() {
-		int r = queue.peek().r;
-		int c = queue.peek().c;
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] { r, c });
-		tempVisited = new boolean[N][M];
-		tempVisited[r][c] = true;
-		cCnt = 1;
-		while (q.size() > 0) {
-			int[] curr = q.poll();
-			for (int i=0; i<4; i++) {
-				int nr = curr[0] + drc[i][0];
-				int nc = curr[1] + drc[i][1];
-				if (map[nr][nc] == 0 || tempVisited[nr][nc]) continue;
-				q.offer(new int[] { nr, nc });
-				tempVisited[nr][nc] = true;
-				cCnt++;
-				if (cCnt == queue.size()) return true;
-			}
-		}
- 		return false;
-	}
-	
-	static void bfs() {
-		int depth = 0;
-		boolean flag = false;
-		while (queue.size() > 0) {
-			if (flag && depth < queue.peek().depth) {
-				if (!check()) {
-					answer = queue.peek().depth;
-					return;
-				}
-			}
+	static void dfs(int r, int c) {
+		cCnt++;
+		for (int i=0; i<4; i++) {
+			int nr = r + drc[i][0];
+			int nc = c + drc[i][1];
+			if (nr < 0 || nc < 0 || nr >= N || nc >= M || map[nr][nc] == 0 || isVisited[nr][nc]) continue;
+			isVisited[nr][nc] = true;
+			dfs(nr, nc);
 			
+		}
+	}
+	
+	static void melt() {
+		int qSize = queue.size();
+		for (int i=0; i<qSize; i++) {
 			Iceberg curr = queue.poll();
-			depth = curr.depth;
-			for (int i=0; i<4; i++) {
-				int nr = curr.r + drc[i][0];
-				int nc = curr.c + drc[i][1];
-				if (map[nr][nc] > 0 || isVisited[nr][nc][depth]) continue;
-				if (curr.height > 0) {
-					curr.height--;
-					map[curr.r][curr.c]--;
-				}
-			}
-			if (curr.height > 0) {
-				queue.offer(new Iceberg(curr.r, curr.c, curr.height, depth + 1));
-				isVisited[curr.r][curr.c][depth + 1] = true;
-			} else flag = true;
+			map[curr.r][curr.c] = (map[curr.r][curr.c] < curr.meltingCnt) ? 0 : map[curr.r][curr.c] - curr.meltingCnt;
+			
+			if (map[curr.r][curr.c] > 0) queue.offer(curr);
 		}
 	}
 
@@ -89,7 +62,6 @@ public class BaekJoon_2573 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
-		isVisited = new boolean[N][M][3001];
 		
 		queue = new LinkedList<>();
 		for (int i=0; i<N; i++) {
@@ -97,15 +69,46 @@ public class BaekJoon_2573 {
 			for (int j=0; j<M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] > 0) {
-					queue.offer(new Iceberg(i, j, map[i][j], 0));
-					isVisited[i][j][0] = true;
+					queue.offer(new Iceberg(i, j, 0));
 				}
 			}
 		}
 		br.close();
 		
-		answer = 0;
-		bfs();
+		int day = 1;
+		while (true) {
+			int qSize = queue.size();
+			for (int i=0; i<qSize; i++) {
+				Iceberg curr = queue.poll();
+				int cnt = 0;
+				for (int j=0; j<4; j++) {
+					int nr = curr.r + drc[j][0];
+					int nc = curr.c + drc[j][1];
+					if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+					
+					if (map[nr][nc] == 0) cnt++;
+				}
+				curr.meltingCnt = cnt;
+				queue.offer(curr);
+			}
+			
+			melt();
+			
+			if (queue.size() == 0) break;
+			if (queue.size() < qSize) {
+				isVisited = new boolean[N][M];
+				cCnt = 0;
+				
+				isVisited[queue.peek().r][queue.peek().c] = true;
+				dfs(queue.peek().r, queue.peek().c);
+				
+				if (cCnt < queue.size()) {
+					answer = day;
+					break;
+				}
+			}
+			day++;
+		}
 		System.out.println(answer);
 	}
 
