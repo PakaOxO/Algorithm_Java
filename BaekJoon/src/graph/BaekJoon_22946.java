4186c5c335a3c2da1cad024c4b7771488fbff7cb
kevin-Arpe <arpeggiogio@gmail.com>
2022-09-28 18:52:04 +0900

    알고리즘 - BaekJoon(Graph) 풀이 업데이트

--- BaekJoon/src/graph/BaekJoon_22946.java
+++ BaekJoon/src/graph/BaekJoon_22946.java
@@ -8,16 +8,23 @@ import java.util.*;
  * @author kevin-Arpe
  * 
  * Sketch Idea
- * 	1. 
+ * 	1. int 오버플로우...
+ * 	2. 먼저 입력받은 원들을 Circle 객체를 만들어 배열에 저장한 뒤, 반지름을 기준으로 오름차순 정렬한다.
+ * 	3. 정렬된 상태에서 앞에서부터 i와 j(i+1~N)을 비교해 포함관계를 체크하고 포함관계가 성립하면 서로 연결관계를 추가한다
+ * 		3.1 이때 i의 반지름이 j의 반지름보다 작으므로 i는 j안에 포함된 상태이다.
+ * 		3.2 포함 되어있는 Circle 객체의 hasOuter 값을 true로 변경한다 이는 나중에 자신을 포함한 원이 없을 때 좌표평면과 연결하기 위한 변수이다.	
+ * 		3.3 자신을 포함하는 부모 원은 단 1개이므로 트리구조의 그래프가 만들어짐을 알 수 있다. 부모는 여러 개의 자식을 가질 수 있으며 최상위 노드는 좌표평면이다.
+ * 	4. 모든 연결정보가 완성되면 좌표평면을 포함(문제의 조건)한 모든 원에서 시작하는 dfs 탐색을 실시해 최대 depth값을 answer에 저장한다.
  *
  */
 public class BaekJoon_22946 {
 	static class Circle {
-		int cNo, x, y, r;
+		int cNo, r;
+		long x, y;
 		Circle prev;
 		boolean hasOuter;
 		
-		Circle(int cNo, int x, int y, int r) {
+		Circle(int cNo, long x, long y, int r) {
 			this.cNo = cNo;
 			this.x = x;
 			this.y = y;
@@ -31,8 +38,9 @@ public class BaekJoon_22946 {
 	static boolean[] isVisited;
 	
 	static boolean isEmbraced(Circle c1, Circle c2) {
-		double dist = Math.sqrt((c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y));
-		return dist - Math.max(c1.r,  c2.r) < 0;
+		double dist = Math.sqrt(((c1.x - c2.x) * (c1.x - c2.x)) + ((c1.y - c2.y) * (c1.y - c2.y)));
+		
+		return dist < c2.r - c1.r;
 	}
 	
 	static void dfs(int curr, int depth) {
@@ -55,7 +63,7 @@ public class BaekJoon_22946 {
 
 		for (int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			Circle circle = new Circle(i + 1, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			Circle circle = new Circle(i + 1, Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()), Integer.parseInt(st.nextToken()));
 			c[i] = circle;
 		}
 		br.close();
@@ -88,7 +96,7 @@ public class BaekJoon_22946 {
 		}
 		
 		answer = 0;
-		for (int i=1; i<=N; i++) {
+		for (int i=0; i<=N; i++) {
 			isVisited[i] = true;
 			dfs(i, 0);
 			isVisited[i] = false;
--- BaekJoon/src/graph/BaekJoon_244948.java
+++ BaekJoon/src/graph/BaekJoon_244948.java
@@ -7,10 +7,28 @@ import java.util.*;
  * BaekJoon_244948, 원 이동하기 2
  * @author kevin-Arpe
  * 
- * Sketch Idea
+ * Sketch Idea (시간 초과)
  * 	1. 교점은 없으므로 두 원간의 관계가 포함인지 미포함관계인지 체크
  * 		1.1 포함관계이면 서로 연결되었다고 가정
  * 		1.2 미포함 관계이면 서로 연결되지 않았으며 이동은 가능하나 중간에 좌표평면(0)을 방문해야 가능
+ * 		1.3 반지름 길이 차이와 중심 간 거리 크기 비교 
+ * 
+ * 
+ * ***** (다른 풀이)
+ * 	1. 트리를 만들기 위해 기존의 N*N 순회를 돌며 포함관계를 체크하는 방법 대신 트리 사용
+ * 
+ * 	2. 중심좌표가 x축만으로 이루어져 있기 때문(y = 0)에 가능
+ * 
+ * 		2.1 중심을 기준으로 좌, 우의 위치를 구해 원의 번호화 함께 x 위치 저장(각 원당 2개, center - r, center + r)
+ * 		2.2 해당 위치들을 List에 넣고 오름차순으로 정렬
+ * 		2.3 정렬된 리스트에서 앞에서 하나씩 꺼내 스택에 넣음
+ * 		2.4 스택에 삽입 시에 스택이 비어있으면 가장 바깥 원이므로 좌표평면과 해당 원을 connect
+ * 		2.5 스택에 다른 원이 있을 땐
+ * 			2.5.1 현재 삽입할 원이랑 top의 원이 같은 원일 때 pop
+ * 			2.5.2 다르다면 이미 들어있던 원 안에 지금 넣을 원이 포함된 구조이므로 서로 connect
+ * 			2.5.3 맨 마지막에 삽입되는 원은 2.5.1에 의해 pop
+ * 
+ * 	3.  모든 connect 트리가 만들어졌으면 시작, 끝 정점을 입력 받아 BFS 탐색을 진행하며 경로 찾음
  *
  */
 public class BaekJoon_244948 {
@@ -33,23 +51,20 @@ public class BaekJoon_244948 {
 	}
 	
 	static class Node {
-		int num;
+		int val;
 		Node prev;
-		Node(int num, Node prev) {
-			this.num = num;
+		
+		Node(int val, Node prev) {
+			this.val = val;
 			this.prev = prev;
 		}
 	}
 	
-	static int N, cnt;
+	static int N;
 	static Circle[] c;
 	static List<Integer>[] conn;
-	static StringBuilder path;
+	static List<Integer> path;
 	
-	static boolean isEmbraced(Circle c1, Circle c2) {
-		double dist = Math.abs(c1.x - c2.x);
-		return dist - Math.max(c1.r, c2.r) < 0;
-	}
 	
 	static void bfs(int start, int end) {
 		Queue<Node> q = new LinkedList<>();
@@ -59,14 +74,12 @@ public class BaekJoon_244948 {
 		
 		while (q.size() > 0) {
 			Node curr = q.poll();
-			for (int next : conn[curr.num]) {
+			for (int next : conn[curr.val]) {
 				if (isVisited[next]) continue;
 				if (next == end) {
-					path.append(next).append(" ");
-					cnt++;
+					path.add(next);
 					while (curr != null) {
-						cnt++;
-						path.append(curr.num).append(" ");
+						path.add(curr.val);
 						curr = curr.prev;
 					}
 					return;
@@ -81,48 +94,68 @@ public class BaekJoon_244948 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		c = new Circle[N];
-		for (int i=0; i<N; i++) {
+		
+		int[][] arr = new int[N*2][2];
+		for (int i=0; i<N*2; i+=2) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			Circle circle = new Circle(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), 0, Integer.parseInt(st.nextToken()));
-			c[i] = circle;
+			
+			int circleNo = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+			
+			arr[i][0] = circleNo;
+			arr[i][1] = x - r;
+			arr[i+1][0] = circleNo;
+			arr[i+1][1] = x + r;
 		}
-		Arrays.sort(c, new Comparator<Circle>() {
+		
+		Arrays.sort(arr, new Comparator<int[]>() {
 			@Override
-			public int compare(Circle o1, Circle o2) {
-				return o1.r - o2.r;
+			public int compare(int[] o1, int[] o2) {
+				return o1[1] - o2[1];
 			}
 		});
 		
-		conn = new ArrayList[N + 1];
+		
+		conn = new ArrayList[N+1];
 		for (int i=0; i<=N; i++) {
 			conn[i] = new ArrayList<>();
 		}
 		
-		for (int i=0; i<N; i++) {
-			for (int j=i+1; j<N; j++) {
-				if (isEmbraced(c[i], c[j])) {
-					conn[c[i].no].add(c[j].no);
-					conn[c[j].no].add(c[i].no);
-					c[i].hasOuter = true;
+		Stack<int[]> stack = new Stack<>();
+		for (int i=0; i<N*2; i++) {
+			int[] point = arr[i];
+			if (stack.size() > 0) {
+				if (stack.peek()[0] == point[0]) {
+					stack.pop();
+				} else {
+					conn[stack.peek()[0]].add(point[0]);
+					conn[point[0]].add(stack.peek()[0]);
+					stack.push(point);
 				}
-				if (c[i].hasOuter) {
-					break;
-				}
-			}
-			if (!c[i].hasOuter) {
-				conn[c[i].no].add(0); 
-				conn[0].add(i + 1);
+			} else {
+				conn[point[0]].add(0);
+				conn[0].add(point[0]);
+				stack.push(point);
 			}
 		}
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int from = Integer.parseInt(st.nextToken());
 		int to = Integer.parseInt(st.nextToken());
+		br.close();
 		
-		path = new StringBuilder();
+		path = new ArrayList<>();
 		bfs(from, to);
-		System.out.println(cnt);
-		System.out.println(path.reverse().toString().trim());
+		
+		StringBuilder sb = new StringBuilder();
+		int pathLen = path.size();
+		sb.append(pathLen).append("\n");
+		for (int i=pathLen-1; i>=0; i--) {
+			if (i > 0) sb.append(path.get(i)).append(" ");
+			else sb.append(path.get(i));
+		}
+		System.out.println(sb);
 	}
 
 }
