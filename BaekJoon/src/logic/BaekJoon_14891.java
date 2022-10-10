a6f1d15bebfd72ef8b128c2f743a5eb3d0ad85ce
kevin-Arpe <arpeggiogio@gmail.com>
2022-10-11 08:14:18 +0900

    알고리즘 - BaekJoon(logic) 풀이 업데이트

--- BaekJoon/src/logic/BaekJoon_14891.java
+++ BaekJoon/src/logic/BaekJoon_14891.java
@@ -12,27 +12,25 @@ import java.util.*;
  *
  */
 public class BaekJoon_14891 {
-	static int[] gear;
+	static char[][] gear;
+	static int[] pointer;
 	static int rCnt;
 	static int[] check;
 	
+	static int right(int gNo) {
+		return (pointer[gNo] + 2) % 8;
+	}
+	
+	static int left(int gNo) {
+		return (pointer[gNo] + 6) % 8;
+	}
+	
 	static void rotate(int gNo, int dir) {
 		if (dir == -1) {
-			if ((gear[gNo] & (1 << 7)) > 0) {
-				gear[gNo] = gear[gNo] << 1;
-				gear[gNo] += 1;
-				gear[gNo] %= (1 << 8);
-			} else {
-				gear[gNo] = gear[gNo] << 1;
-				gear[gNo] %= (1 << 8);
-			}
+			pointer[gNo] = (pointer[gNo] + 1) % 8;
 		} else {
-			if ((gear[gNo] & 1) > 0) {
-				gear[gNo] = gear[gNo] >> 1;
-				gear[gNo] += (1 << 7);
-			} else {
-				gear[gNo] = gear[gNo] >> 1;
-			}
+			pointer[gNo]--;
+			if (pointer[gNo] < 0) pointer[gNo] += 8;;
 		}
 	}
 	
@@ -40,16 +38,61 @@ public class BaekJoon_14891 {
 		check = new int[4];
 		check[gNo] = dir;
 		
+		if (gNo == 0) {
+			if (gear[0][right(0)] != gear[1][left(1)]) {
+				check[1] = dir*-1;
+				if (gear[1][right(1)] != gear[2][left(2)]) {
+					check[2] = dir;
+					if (gear[2][right(2)] != gear[3][left(3)]) {
+						check[3] = dir*-1;
+					}
+				}
+			}
+		} else if (gNo == 1) {
+			if (gear[1][right(1)] != gear[2][left(2)]) {
+				check[2] = dir*-1;
+				if (gear[2][right(2)] != gear[3][left(3)]) {
+					check[3] = dir;
+				}
+			}
+			
+			if (gear[1][left(1)] != gear[0][right(0)]) {
+				check[0] = dir*-1;
+			}
+		} else if (gNo == 2) {
+			if (gear[2][right(2)] != gear[3][left(3)]) {
+				check[3] = dir*-1;
+			}
+			
+			if (gear[2][left(2)] != gear[1][right(1)]) {
+				check[1] = dir*-1;
+				if (gear[1][left(1)] != gear[0][right(0)]) {
+					check[0] = dir;
+				}
+			}
+		} else {
+			if (gear[3][left(3)] != gear[2][right(2)]) {
+				check[2] = dir*-1;
+				if (gear[2][left(2)] != gear[1][right(1)]) {
+					check[1] = dir;
+					if (gear[1][left(1)] != gear[0][right(0)]) {
+						check[0] = dir*-1;
+					}
+				}
+			}
+		}
 	}
 	
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		gear = new int[4];
-		gear[0] = Integer.parseInt(br.readLine(), 2);
-		gear[1] = Integer.parseInt(br.readLine(), 2);
-		gear[2] = Integer.parseInt(br.readLine(), 2);
-		gear[3] = Integer.parseInt(br.readLine(), 2);
+		gear = new char[4][8];
+		gear[0] = br.readLine().toCharArray();
+		gear[1] = br.readLine().toCharArray();
+		gear[2] = br.readLine().toCharArray();
+		gear[3] = br.readLine().toCharArray();
+		
+		pointer = new int[4];
 
 		rCnt = Integer.parseInt(br.readLine());
 		
@@ -61,17 +104,16 @@ public class BaekJoon_14891 {
 			check(gNo, dir);
 			for (int j=0; j<4; j++) {
 				if (check[j] == 0) continue;
-				rotate(j, dir);
+				rotate(j, check[j]);
 			}
 		}
 		br.close();
 		
-		for (int g : gear) System.out.println(Integer.toBinaryString(g));
 		int answer = 0;
-		if ((gear[0] & (1 << 7)) > 0) answer += 1;
-		if ((gear[1] & (1 << 7)) > 0) answer += 2;
-		if ((gear[2] & (1 << 7)) > 0) answer += 4;
-		if ((gear[3] & (1 << 7)) > 0) answer += 8;
+		if (gear[0][pointer[0]] == '1') answer += 1;
+		if (gear[1][pointer[1]] == '1') answer += 2;
+		if (gear[2][pointer[2]] == '1') answer += 4;
+		if (gear[3][pointer[3]] == '1') answer += 8;
 		
 		System.out.println(answer);
 	}
