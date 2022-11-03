ffa245a76635736c45158f90a090e08c48bf6ae1
kevin-Arpe <arpeggiogio@gmail.com>
2022-11-03 23:20:57 +0900

    알고리즘 - BaekJoon(divideAndConquer) 풀이 업데이트

--- BaekJoon/src/divideAndConquer/BaekJoon_5639.java
+++ BaekJoon/src/divideAndConquer/BaekJoon_5639.java
@@ -8,21 +8,40 @@ import java.util.*;
  * @author kevin-Arpe
  * 
  * Sketch Idea
- * 	1. 일단 맨 앞에서 시작(target), target을 정답 배열 맨 뒤 인덱스에 추가하고 뒤 인덱스 갱신(pointer-1)
- * 	2. target을 기준으로 큰 숫자가 나올 때까지 우측으로 탐색
- * 		2.1 큰 숫자가 나오면 해당 숫자부터 target 기준 right, 그 이전 숫자들을 left
- * 		2.2 right가 있다면 right의 첫 숫자를  pointer 위치에 넣고
- * 		2.3 없다면 left의 첫 숫자를 pointer에 넣음
- * 		2.4 넣은 숫자를 새로운 target으로 다시 새로운 left, right를 찾음
- * 		2.5 만약 배열 끝까지 갔는데 못찾으면 다시 배열 맨 앞에서부터 target으로 시작
+ * 	1. 처음 입력 배열(preOrder)의 왼쪽 끝 인덱스를 left, 오른쪽 끝 인덱스를 right로 재귀 시작
+ *  2. 재귀 시작시 left의 preOrder 값을 pointer(postOrder 뒤쪽 인덱스)의 위치에 저장
+ * 	3. 이후 현재의 left 숫자를 기준으로 더 큰 숫자가 나올 때까지 우측으로 탐색
+ * 		2.1 큰 숫자가 나오면 해당 위치를 mid(초기값 -1)라 생각하고
+ *          2.1.1 해당 숫자를 left, 끝의 숫자를 right로 하는 재귀 호출
+ *          2.1.2 그리고 현재의 left + 1을 left로 그리고 mid - 1을 right로 하는 재귀 호출
+ * 		2.2 큰 숫자가 나오지 않으면 현재의 left + 1을 left로 right를 right로 하는 재귀 호출
+ * 
+ *  4. 짜잔 완성
  *
  */
 public class BaekJoon_5639 {
+    static int N, pointer;
     static List<Integer> preOrder;
     static int[] postOrder;
     
-    static void getPostOrder() {
+    static void getPostOrder(int left, int right) {
+        if (left > right || pointer < 0) return;
+        postOrder[pointer--] = preOrder.get(left);
         
+        int mid = -1;
+        for (int i=left+1; i<=right; i++) {
+            if (preOrder.get(i) > preOrder.get(left)) {
+                mid = i;
+                break;
+            }
+        }
+        
+        if (mid != -1) {
+            getPostOrder(mid, right);
+            getPostOrder(left + 1, mid - 1);
+        } else {
+            getPostOrder(left + 1, right);
+        }
     }
 
 	public static void main(String[] args) throws Exception {
@@ -30,21 +49,21 @@ public class BaekJoon_5639 {
 	    String str;
 	    
 	    preOrder = new ArrayList<>();
-	    while (true) {
-	        str = br.readLine();
+	    while ((str = br.readLine()) != null && str.length() > 0) {
 	        if (str == null || str.length() == 0) break;
 	        preOrder.add(Integer.parseInt(str));
 	    }
+	    br.close();
 	    
-	    System.out.println(preOrder);
-	    int N = preOrder.size();
+	    N = preOrder.size();
 	    postOrder = new int[N];
 	    
-	    int target = 0;
-	    int res = N;
-	    while (res > 0) {
-	        
-	    }
+	    pointer = N - 1;
+	    getPostOrder(0, N - 1);
+	    
+	    StringBuilder sb = new StringBuilder();
+	    for (int n : postOrder) sb.append(n).append("\n");
+	    System.out.print(sb);
 	}
 
 }
---
+++ BaekJoon/src/recursive/BaekJoon_2448.java
@@ -0,0 +1,22 @@
+package recursive;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * BaekJoon_2448, 별 찍기-11
+ * @author kevin-Arpe
+ * 
+ * Sketch Idea
+ *  1. 
+ *
+ */
+public class BaekJoon_2448 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+    }
+
+}
