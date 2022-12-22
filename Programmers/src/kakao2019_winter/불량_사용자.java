137366400a5af694882a509d3d23076ef30abfd6
kevin-Arpe <arpeggiogio@gmail.com>
2022-12-22 23:54:56 +0900

    Programmers - 알고리즘 풀이 업데이트

--- Programmers/src/kakao2019_winter/불량_사용자.java
+++ Programmers/src/kakao2019_winter/불량_사용자.java
@@ -1,14 +1,38 @@
 package kakao2019_winter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class 불량_사용자 {
+	static String[] sel;
+	static Set<Integer> set;
 	
-	public static int solution(String[] user_id, String[] banned_id) {
-        int answer = 0;
-        
-        return answer;
-    }
-	
-	public static void main(String[] args) {
-		System.out.println(solution(new String[] {"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[] {"fr*d*", "abc1**"}));
+	static boolean check(String target, String banned) {
+		if (target.length() != banned.length()) return false;
+		
+		for (int i=0; i<target.length(); i++) {
+			if (banned.charAt(i) == '*') continue;
+			if (banned.charAt(i) != target.charAt(i)) return false;
+		}
+		return true;
 	}
+	
+	static void dfs(String[] targets, String[] banned, int idx, int bit) {
+		if (idx == banned.length) {
+			set.add(bit);
+			return;
+		}
+		
+		for (int i=0; i<targets.length; i++) {
+			if ((bit & 1 << i) > 0) continue;
+			if (check(targets[i], banned[idx]))
+				dfs(targets, banned, idx + 1, (bit | 1 << i));
+		}
+	}
+	
+	public int solution(String[] user_id, String[] banned_id) {
+        set = new HashSet<>();
+        dfs(user_id, banned_id, 0, 0);
+        return set.size();
+    }
 }
