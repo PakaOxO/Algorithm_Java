b259329ef208912753ce233c91f6e91c77a1b6dd
kevin-Arpe <arpeggiogio@gmail.com>
2023-03-10 21:26:02 +0900

    Programmers - 알고리즘 풀이 업데이트

---
+++ Programmers/src/level01/Programmers_음양더하기.js
@@ -0,0 +1,8 @@
+function solution(absolutes, signs) {
+  var answer = 0;
+  const len = absolutes.length;
+  for (let i = 0; i < len; i++) {
+    answer += signs[i] ? absolutes[i] : -absolutes[i];
+  }
+  return answer;
+}
