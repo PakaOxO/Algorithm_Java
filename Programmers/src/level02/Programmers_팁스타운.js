a375c969af0966f2cfc5c7621d3bb238de2fbc94
kevin-Arpe <arpeggiogio@gmail.com>
2023-03-23 21:41:47 +0900

    Algorithm - Programmers 문제풀이 업로드

--- Programmers/src/level02/Programmers_팁스타운.js
+++ Programmers/src/level02/Programmers_팁스타운.js
@@ -1,10 +1,9 @@
 function solution(s) {
-  var answer = -1;
   const len = s.length;
   const stack = new Stack();
   for (let i = 0; i < len; i++) {
     const c = s.charAt(i);
-    if (stack.getSize() < 1) {
+    if (stack.top < 1) {
       stack.push(c);
     } else {
       if (stack.peek() === c) {
@@ -15,34 +14,27 @@ function solution(s) {
     }
   }
 
-  return stack.getSize() === 0 ? 1 : 0;
+  return stack.top === 0 ? 1 : 0;
 }
 
 class Stack {
   constructor() {
     this.arr = [];
     this.top = 0;
-    this.size = 0;
   }
 
   push(val) {
     this.arr[this.top++] = val;
-    this.size++;
   }
 
   pop() {
-    if (this.size === 0) return null;
-    this.size--;
+    if (this.top === 0) return null;
     return this.arr[--this.top];
   }
 
   peek() {
-    if (this.size === 0) return null;
+    if (this.top === 0) return null;
     return this.arr[this.top - 1];
-  }
-
-  getSize() {
-    return this.size;
   }
 }
 
