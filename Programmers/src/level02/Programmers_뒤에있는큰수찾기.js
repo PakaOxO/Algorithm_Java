46471c267e65bce951e53aeb8be732cbaaaee97f
kevin-Arpe <arpeggiogio@gmail.com>
2023-03-25 20:42:01 +0900

    Algorithm - Programmers 문제풀이 업로드

--- Programmers/src/level02/Programmers_뒤에있는큰수찾기.js
+++ Programmers/src/level02/Programmers_뒤에있는큰수찾기.js
@@ -1,23 +1,27 @@
 function solution(numbers) {
   const len = numbers.length;
-  const dp = Array.from(Array(len), () => new Array(2));
+  const dp = new Array(len);
   const answer = new Array(len);
 
-  const find = (curr, target) => {
-    if (target >= len) {
-      return -1;
+  const find = (val, next) => {
+    // 다음 원소가 범위 밖이면 내가 제일 큼
+    if (next >= len) {
+      return [-1, len];
     }
 
-    if (numbers[curr] < numbers[target]) {
-      return numbers[target];
+    if (val < numbers[next]) {
+      // 다음 원소보다 내가 작으면 다음 원소가 뒷 큰수
+      return [numbers[next], next];
     } else {
-      find(target, dp[target]);
+      // 내가 다음 원소보다 같거나 크면 다음 원소로 넘어가서 뒷 큰수를 찾음
+      return find(val, dp[next]);
     }
   };
-
+  // 맨 뒤 원소부터 시작
   let pointer = len - 1;
   while (pointer >= 0) {
-    dp[pointer] = find(pointer, pointer + 1);
+    [answer[pointer], dp[pointer]] = find(numbers[pointer], pointer + 1);
+    pointer--;
   }
   return answer;
 }
