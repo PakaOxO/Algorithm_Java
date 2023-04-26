f9676dfda169a649dcf516f7002fff9cc60864c7
kevin-Arpe <arpeggiogio@gmail.com>
2023-04-26 22:04:11 +0900

    Algorithm - Programmers 문제풀이 업로드

---
+++ Programmers/src/level02/전화번호_목록.java
@@ -0,0 +1,32 @@
+package level02;
+
+import java.util.Arrays;
+
+class Solution {
+	private boolean check(String s1, String s2) {
+		int len = s1.length();
+    	for (int j=0; j<len; j++) {        		
+    		if (s1.charAt(j) != s2.charAt(j)) return false;
+    	}
+    	return true;
+	}
+
+    public boolean solution(String[] phone_book) {
+    	Arrays.sort(phone_book);
+    	
+        for (int i=1; i<phone_book.length; i++) {
+        	if (check(phone_book[i - 1], phone_book[i])) return false;
+        }
+        return true;
+    }
+}
+
+public class 전화번호_목록 {
+	public static void main(String[] args) throws Exception {
+		Solution s = new Solution();
+		s.solution(new String[] {"119", "97674223", "1195524421"});
+		s.solution(new String[] {"123","456","789"});
+		s.solution(new String[] {"12","123","1235","567","88"});
+	}
+
+}
---
+++ Programmers/src/level02/피로도.java
@@ -0,0 +1,35 @@
+package level02;
+
+class Solution2 {
+	int answer = 0;
+	int len = 0;
+	boolean[] isVisited;
+	int[][] d;
+	
+	private void dfs(int k, int cnt) {
+		answer = Math.max(answer, cnt);
+		for (int i=0; i<len; i++) {
+			if (k < d[i][0] || k < d[i][1] || isVisited[i]) continue;
+			isVisited[i] = true;
+			dfs(k - d[i][1], cnt + 1);
+			isVisited[i] = false;
+		}
+	}
+	
+    public int solution(int k, int[][] dungeons) {
+    	d = dungeons;
+    	len = d.length;
+    	isVisited = new boolean[len];
+    	
+    	dfs(k, 0);
+        return answer;
+    }
+}
+
+public class 피로도 {
+	public static void main(String[] args) throws Exception {
+		Solution2 s = new Solution2();
+		System.out.println(s.solution(80, new int[][] {{80,20},{50,40},{30,10}}));
+	}
+
+}
