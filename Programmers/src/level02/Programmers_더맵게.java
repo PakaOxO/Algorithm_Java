ea51a5788ac0e1394f73f0c5eb0fd12436e46997
PakaOxO <57397003+PakaOxO@users.noreply.github.com>
2023-05-22 15:51:11 +0900

    Algorithm - Programmers 문제풀이 업로드

---
+++ Programmers/src/level02/Programmers_더맵게.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+
+public class Programmers_더맵게 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[] {1, 2, 3, 9, 10, 12}, 7));
+    }
+}
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+
+        int answer = 0;
+        while (pq.size() > 1 && (int)pq.peek() < K) {
+            int min = pq.poll();
+            int min2 = pq.poll();
+            pq.offer(min + (min2 * 2));
+            answer++;
+        }
+        return pq.peek() >= K ? answer : -1;
+    }
+}
