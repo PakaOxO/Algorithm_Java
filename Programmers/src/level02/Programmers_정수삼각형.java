7d6ae335c5d4aa7bf6bc1a6d5f9b0f5f45398715
PakaOxO <57397003+PakaOxO@users.noreply.github.com>
2023-05-25 16:02:14 +0900

    Algorithm - 프로그래머스 문제풀이 업로드

---
+++ Programmers/src/level02/Programmers_정수삼각형.java
@@ -0,0 +1,31 @@
+import java.util.PriorityQueue;
+
+public class Programmers_정수삼각형 {
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.solution(new int[][] {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}));
+    }
+}
+
+class Solution2 {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int N = triangle.length;
+        int[][] dp = new int[N][];
+        for (int i=0; i<N; i++) {
+            dp[i] = new int[i + 1];
+        }
+
+        for (int i=N-1; i>=0; i--) {
+            for (int j=0; j<=i; j++) {
+                if (i == N - 1) {
+                    dp[i][j] = triangle[i][j];
+                    continue;
+                }
+                dp[i][j] = triangle[i][j] + Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+
+        return dp[0][0];
+    }
+}
