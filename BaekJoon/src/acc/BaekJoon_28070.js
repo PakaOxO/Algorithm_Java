1e3d186a5b09ce5f8e8f6ee5fe424a745b79f20e
kevin-Arpe <arpeggiogio@gmail.com>
2023-05-27 00:06:11 +0900

    Algorithm - 백준 데일리문제 업로드

--- BaekJoon/src/acc/BaekJoon_28070.js
+++ BaekJoon/src/acc/BaekJoon_28070.js
@@ -1,72 +1,48 @@
 /*
   BaekJoon_28070, 유니의 편지 쓰기
   Sketch Idea
-    1. 
+    1. 누적합을 떠올리지 못해서 한참 고민했던 문제
+    2. 연도 파싱 부분에서 문제를 찾지 못해 잘못된 코드를 작성하고 있었음.
 */
 const solution = () => {
   let fs = require("fs");
   let input = fs.readFileSync("./dev/stdin/28070.txt").toString().split("\n");
 
-  /* 초기값 */
-  const N = +input[0];
-  const dates = [];
-  const isIn = [false];
-  const enter = [];
-  const exit = [];
-
-  for (let i = 1; i <= N; i++) {
-    const date = input[i]
-      .split(" ")
-      .map((item) => item.split("-").map((str) => +str));
-    dates.push([...date, false]);
-    enter.push([...date[0], i]);
-    exit.push([...date[1], i]);
-    isIn.push(false);
-  }
-
   /* 메서드 */
-  const compareDate = (d1, d2) => {
-    if (d1[0] === d2[0]) return d1[1] - d2[1];
-    return d1[0] - d2[0];
+  const dateToIdx = (year, month) => {
+    return (year - 2000) * 12 + month - 1;
   };
 
-  enter.sort((a, b) => {
-    return compareDate(a, b);
-  });
-  exit.sort((a, b) => {
-    return compareDate(a, b);
-  });
+  const idxToDate = (idx) => {
+    const share = Math.floor(idx / 12) + 2000;
+    const res = (idx % 12) + 1;
+    return share + "-" + (res < 10 ? "0" : "") + res;
+  };
 
-  let answer = 0;
-  let [max, count] = [0, 0];
-  let [pointer1, pointer2] = [0, 0];
+  /* 초기값 */
+  const N = +input[0];
+  const maxL = (9999 - 2000 + 1) * 12 + 1;
+  const acc = new Array(maxL).fill(0);
 
-  while (pointer1 < N && pointer2 < N) {
-    count++;
-    isIn[pointer1] = true;
+  for (let i = 1; i <= N; i++) {
+    const [from, to] = input[i]
+      .split(" ")
+      .map((item) => item.split("-").map((str) => +str));
 
-    let hasExit = false;
-    if (compareDate(enter[pointer1], exit[pointer2]) >= 0 && isIn[pointer2]) {
-      isIn[pointer2] = false;
-      if (compareDate(enter[pointer1], exit[pointer2]) === 0) {
-        hasExit = true;
-      } else {
-        count--;
-        pointer2++;
-      }
+    const fromIdx = dateToIdx(from[0], from[1]);
+    const toIdx = dateToIdx(to[0], to[1]);
+    acc[fromIdx]++;
+    acc[toIdx + 1]--;
+  }
+
+  let answer = idxToDate(0);
+  let max = acc[0];
+  for (let i = 1; i < maxL; i++) {
+    acc[i] += acc[i - 1];
+    if (acc[i] > max) {
+      max = acc[i];
+      answer = idxToDate(i);
     }
-
-    if (count > max) {
-      max = count;
-      answer = enter[pointer1][0] + "-" + enter[pointer1][1];
-    }
-    max = Math.max(max, count);
-
-    if (hasExit) {
-      count--;
-      pointer2++;
-    }
-    pointer1++;
   }
 
   return answer;
