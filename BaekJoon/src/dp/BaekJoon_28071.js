95b4ab41d79997fe6fb305e6c35e962b78258d3e
kevin-Arpe <arpeggiogio@gmail.com>
2023-06-17 00:02:02 +0900

    Algorithm - BaekJoon 데일리 문제 풀이 업로드

--- BaekJoon/src/dp/BaekJoon_28071.js
+++ BaekJoon/src/dp/BaekJoon_28071.js
@@ -4,23 +4,37 @@ const solution = () => {
 
   const [N, M, K] = input[0].split(" ").map((item) => +item);
   const candies = input[1].split(" ").map((item) => +item);
+  candies.sort((a, b) => a - b);
 
-  let answer = 0;
   const dp = [];
   dp.push(new Array(K).fill(0));
 
-  const bfs = (box, res) => {
-    return 0;
+  const getDp = (box) => {
+    for (let i = 0; i < K; i++) {
+      for (let j = 0; j < K; j++) {
+        const res = (i + j) % K;
+        if (dp[box - 1][i] < 1 || dp[1][j] < 1) continue;
+        dp[box][res] = Math.max(dp[box][res], dp[box - 1][i] + dp[1][j]);
+      }
+    }
   };
 
+  let answer = 0;
   for (let i = 1; i <= M; i++) {
-    dp[i] = [];
-    for (let j = 0; j < K; j++) {
-      dp[i].push(bfs(i, j));
+    dp.push(new Array(K).fill(0));
+
+    if (i === 1) {
+      for (let j = 0; j < N; j++) {
+        const res = candies[j] % K;
+        dp[i][res] = candies[j];
+      }
+    } else {
+      getDp(i);
     }
+    answer = Math.max(answer, dp[i][0]);
   }
 
-  return answer;
+  return answer < K ? 0 : answer;
 };
 
 console.log(solution());
