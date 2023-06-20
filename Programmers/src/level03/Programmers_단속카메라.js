8d05a317403f5cd6a940d72d050462d42a0b0653
kevin-Arpe <arpeggiogio@gmail.com>
2023-06-20 20:40:55 +0900

    Algorithm - Programmers 데일리 문제 풀이 업로드

--- Programmers/src/level03/Programmers_단속카메라.js
+++ Programmers/src/level03/Programmers_단속카메라.js
@@ -1,21 +1,14 @@
 const solution = (routes) => {
-  let size = 0;
-  routes.forEach((r) => {
-    size = Math.max(size, Math.abs(r[0]), Math.abs(r[1]));
-  });
+  const N = routes.length;
+  routes.sort((a, b) => a[1] - b[1]);
 
-  const acc = new Array(size * 2 + 2).fill(0);
-
-  routes.forEach((r) => {
-    acc[r[0] + size]++;
-    acc[r[1] + size + 1]--;
-  });
-
-  for (let i = 1; i < size * 2 + 2; i++) {
-    acc[i] += acc[i - 1];
-  }
-  console.log(acc);
+  let pos = -30001;
   let answer = 0;
+  for (let i = 0; i < N; i++) {
+    if (pos >= routes[i][0] && pos <= routes[i][1]) continue;
+    pos = routes[i][1];
+    answer++;
+  }
 
   return answer;
 };
