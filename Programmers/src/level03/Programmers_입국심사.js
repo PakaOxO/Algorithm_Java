dadbbc9144f7822431259dbd35b051013b3cce63
kevin-Arpe <arpeggiogio@gmail.com>
2023-08-28 18:21:33 +0900

    [TIL] Algorithm - Programmers 데일리 문제 업로드

--- Programmers/src/level03/Programmers_입국심사.js
+++ Programmers/src/level03/Programmers_입국심사.js
@@ -2,7 +2,10 @@
  * Programmers_입국 심사
  *  1. 문제 분류 : 이분 탐색
  *  2. 접근 방법
- *    -
+ *    - 시간의 범위 (1 ~ 10억)를 이분 탐색하면서 해당 시간에 소화할 수 있는 사람의 수를 카운트
+ *    - 사람 수가 n보다 같거나 많으면 시간이 널널한 것이므로 right <- mid - 1
+ *    - 사람 수가 n보다 적으면 시간이 부족한 것이므로 left <- mid + 1
+ *    - 사람 수 n을 소화할 수 있는 최소값이 필요하므로 lowerbound left값을 반환
  */
 const solution = (n, times) => {
   /* 변수 초기화 */
@@ -36,9 +39,8 @@ const solution = (n, times) => {
       if (sum >= n) return true;
     }
 
-    return sum >= n;
+    return sum > n;
   }
 };
 
 console.log(solution(6, [7, 10]));
-
--- Programmers/src/level04/Programmers_징검다리.js
+++ Programmers/src/level04/Programmers_징검다리.js
@@ -2,13 +2,18 @@
  * Programmers_징검다리
  *    1. 문제 분류 : 이분 탐색
  *    2. 접근 방법
- *      -
+ *      - 바위 사이의 거리는 1~10억
+ *      - 바위 사이의 거리를 이분 탐색하며 해당 거리가 최소 간격이라 가정했을 때 제거해야하는 바위의 개수를 구함
+ *        -> 제거해야 할 바위의 개수가 n보다 많으면 거리가 긴 것이기 때문에 right <- mid - 1
+ *        -> 제거해야 할 바위의 개수가 n보다 같거나 적으면 거리가 너무 짧은 것이기 때문에 left <- mid + 1
+ *      - lowerbound로 이분 탐색 종료시 right의 값이 조건을 만족하는 시작값(최소값)
  */
 const solution = (distance, rocks, n) => {
   /* 변수 초기화 */
   const R = rocks.length;
 
   /* 정답 반환 */
+  rocks.sort((a, b) => a - b);
   return binarySeacrh(1, distance);
 
   /* 이분 탐색 */
@@ -21,7 +26,7 @@ const solution = (distance, rocks, n) => {
         left = mid + 1;
       }
     }
-    return left;
+    return right;
   }
 
   /* 거리가 주어졌을 때 사라지는 바위의 개수 */
@@ -36,13 +41,12 @@ const solution = (distance, rocks, n) => {
       }
     }
 
-    if (distance - queue[queue.length - 1] < dist) {
+    if (queue[queue.length - 1] !== 0 && distance - queue[queue.length - 1] < dist) {
       count++;
     }
 
-    return count <= n;
+    return count > n;
   }
 };
 
 console.log(solution(25, [2, 14, 11, 21, 17], 2));
-
