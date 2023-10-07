bd3cf286609bc315ecf6c8fc7e022017a4c05b2f
kevin-Arpe <arpeggiogio@gmail.com>
2023-10-07 23:17:43 +0900

    [TIL]Algorithm: 데일리 문제

--- Programmers/src/level03/Programmers_외벽점검.java
+++ Programmers/src/level03/Programmers_외벽점검.java
@@ -1,12 +1,11 @@
 package level03;
 
-import java.util.Arrays;
-
 public class Programmers_외벽점검 {
 	public static void main(String[] args) {
 		Solution_외벽점검 s = new Solution_외벽점검();
 		
 		System.out.println(s.solution(12, new int[] { 1, 5, 6, 10 }, new int[] { 1, 2, 3, 4 }));
+		System.out.println(s.solution(12, new int[] { 1, 3, 4, 9, 10 }, new int[] { 3, 5, 7 }));
 	}
 }
 
@@ -15,10 +14,9 @@ public class Programmers_외벽점검 {
  *		- 문제 분류: 그리디 
  */
 class Solution_외벽점검 {
-	int MAX_DIST = 8;
+	int MAX_DIST = 100;
 	int N, M, D;
 	int[] weak, dist;
-	boolean[] map;
 	boolean[] visited;
 	int[] dCount;
 	int answer;
@@ -30,7 +28,6 @@ class Solution_외벽점검 {
 		D = dist.length;
 		this.weak = weak;
 		this.dist = dist;
-		map = new boolean[N];
 		visited = new boolean[M];
 		dCount = new int[MAX_DIST + 1];
 		answer = D + 1;
@@ -41,56 +38,51 @@ class Solution_외벽점검 {
 			dCount[dist[i]]++;
 		}
 		
-		for (int i=0; i<M; i++) {
-			map[weak[i]] = true;
-		}
-		
 		// 시작 지점 
 		for (int i=0; i<M; i++) {
 			dfs(i, 0);
 		}
 		
 		/* 정답 반환 */
-		return answer;
+		return answer > D ? -1 : answer;
 	}
 	
 	private void dfs(int pos, int use) {
 		if (use >= answer) return;
 		if (visited[pos]) {
-			answer = Math.max(answer, use);
+			answer = Math.min(answer, use);
 			return;
 		}
-		System.out.println(pos + " " + use + " " + Arrays.toString(visited));
 		
 		for (int i=MAX_DIST; i>0; i--) {
 			if (dCount[i] == 0) continue;
-			int end = pos;
+			int end = (pos + 1) % M;
 			
 			while (true) {
-				int d = (weak[end] - weak[pos] + N) / N;
-				if (visited[end] || d > i || end == pos) {
-					end = (end - 1 + N) / N;
+				int d = (weak[end] - weak[pos] + N) % N;
+				if (end == pos || visited[end] || d > i) {
+					end = (end - 1 + M) % M;
 					break;
 				}
-				end = (end + 1) / M;
+				end = (end + 1) % M;
 			}
 			
 			int j = pos;
 			while (true) {
 				visited[j] = true;
 				if (j == end) break;
-				j = (j + 1) / M;
+				j = (j + 1) % M;
 			}
 			
 			dCount[i]--;
-			dfs((end + 1) / M, use + 1);
+			dfs((end + 1) % M, use + 1);
 			dCount[i]++;
 			
 			j = pos;
 			while (true) {
 				visited[j] = false;
 				if (j == end) break;
-				j = (j + 1) / N;
+				j = (j + 1) % M;
 			}
 		}
 	}
