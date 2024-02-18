aaf9d3a1c561e37f1da494f0efed8f54ebd0002b
kevin-Arpe <arpeggiogio@gmail.com>
2024-02-19 00:02:41 +0900

    add: 데일리 알고리즘

--- BaekJoon/src/dp/BaekJoon_16456.js
+++ BaekJoon/src/dp/BaekJoon_16456.js
@@ -6,23 +6,19 @@ const solution = () => {
   /* 변수 관리 */
   const N = +require("fs").readFileSync("./dev/stdin/16456.txt").toString().trim();
   const dp = Array.from({ length: N }, () => 0);
-  const v = Array.from({ length: N }, () => false);
   const INF = 1000000009;
 
   /* 메인 로직 */
-  if (N >= 1) dp[N - 1] = 1;
-  if (N >= 2) dp[N - 2] = 1;
+  dp[0] = 1;
+  if (N > 1) dp[1] = 1;
+  if (N > 2) dp[2] = 1;
 
-  for (let i = N - 1; i >= 0; i--) {
-    if (i + 1 < N) dp[i + 1] = (dp[i + 1] + dp[i]) % INF;
-    if (i - 1 >= 0) dp[i - 1] = (dp[i - 1] + dp[i]) % INF;
-    if (i - 2 >= 0) dp[i - 2] = (dp[i - 2] + dp[i]) % INF;
+  for (let i = 3; i < N; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 3]) % INF;
   }
-  console.log(dp);
 
   /* 정답 반환 */
-  return dp[0];
+  return N > 2 ? (dp[N - 1] + dp[N - 3]) % INF : dp[N - 1];
 };
 
 console.log(solution());
-
